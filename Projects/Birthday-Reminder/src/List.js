d2072f6910a4b865acd0228e610cb1cce3802b7d
dawkey95 <dawidkeyser95@gmail.com>
2022-11-14 10:27:29 +1300

    Section 4: Lesson 47 Completed

--- Projects/Birthday-Reminder/src/List.js
+++ Projects/Birthday-Reminder/src/List.js
@@ -5,6 +5,7 @@ const List = ({ people }) => {
 		<>
 			{people.map((person) => {
 				const { id, name, age, image } = person;
+
 				return (
 					<article key={id} className="person">
 						<img src={image} alt={name} />
